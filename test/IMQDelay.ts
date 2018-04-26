9d95e34eba24fba0ba1f7061f3bc5ae6afe4cba6
Mykhailo Stadnyk <mikhus@gmail.com>
2018-04-26 23:15:58 +0300

    WIP: unit tests

--- test/IMQDelay.ts
+++ test/IMQDelay.ts
@@ -22,4 +22,15 @@ describe('IMQDelay', () => {
     it('should be a class', () => {
         expect(typeof IMQDelay).to.equal('function');
     });
+
+    describe('constructor()', () => {
+        it('should construct ms time from given args', () => {
+            expect(new IMQDelay(5).ms).to.equal(5);
+            expect(new IMQDelay(5, 'ms').ms).to.equal(5);
+            expect(new IMQDelay(5, 's').ms).to.equal(5000);
+            expect(new IMQDelay(5, 'm').ms).to.equal(300000);
+            expect(new IMQDelay(5, 'h').ms).to.equal(3600*5000);
+            expect(new IMQDelay(5, 'd').ms).to.equal(86400*5000);
+        });
+    });
 });
